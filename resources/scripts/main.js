f05959013cf88336822b6916fd40de83cbb6b59b
Mac User <sabbey37@gmail.com>
2017-07-27 12:06:49 -0400

    Add google geocode URL

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,4 +1,17 @@
 var $mainMap = $('[data-role="main-map"]');
+var $searchField = $('[data-role="search-form"]');
+var GEOCODE = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+var GOOGLE_API_KEY = "AIzaSyD8UFO6YBOxOpaAG0Q6BUg4iqd_9214ZWY";
+
+
+getGeoCoords(searchValue) {
+    var formattedSearchValue = searchValue.split(' ');
+    formattedSearchValue = searchValue.join("+");
+    var req = $.get(GEOCODE + formattedSearchValue + "&key=" + GOOGLE_API_KEY);
+    console.log(req);
+} 
+
+}
 
 var map;
 function initMap() {
@@ -15,7 +28,7 @@ function addSearchListener() {
     $searchField.submit(function (event){
         event.preventDefault();
         var searchValue = $('[data-role="search"]').val();
-        console.log(searchValue);
         getGeoCoords(searchValue);
-    })
+    });
 }
+
