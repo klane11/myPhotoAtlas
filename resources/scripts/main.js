340f5d1c793224c0b5f6fd9e99a9ba2608531f0a
Mac User <sabbey37@gmail.com>
2017-07-28 10:16:52 -0400

    Search by text

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -6,9 +6,8 @@ var FLICKR_API_KEY = "566ab7296356eb73e65e0d7f80743bde";
 var $pictureDisplay = $('[data-role="picture-display"]');
 
 function getGeoCoords(searchValue) {
-    var formattedSearchValue = searchValue.split(' ');
-    formattedSearchValue = formattedSearchValue.join("+");
-    var resp = $.get(GEOCODE + formattedSearchValue + "&key=" + GOOGLE_API_KEY);
+    var URI = encodeURI(searchValue);
+    var resp = $.get(GEOCODE + URI + "&key=" + GOOGLE_API_KEY);
     resp
         .then(photoSearch)
 }
@@ -22,15 +21,22 @@ function makePicture(farmID, serverID, photoID, secret, title) {
 }
 
 
-function photoSearch(resp) {
+function photoSearch(searchValue) {
     if ($pictureDisplay.children()) {
         $pictureDisplay.empty();
     }
-    // console.log(resp["results"][0]["geometry"]["location"]["lng"]);
-    // console.log(resp["results"][0]["geometry"]["location"]["lat"]);
-    var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&lat=" + resp["results"][0]["geometry"]["location"]["lat"] + "&lon=" + resp["results"][0]["geometry"]["location"]["lng"] + "&sort=faves&format=json&nojsoncallback=1");
+
+    // Gets search results by latitude and longitude
+    // var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&lat=" + resp["results"][0]["geometry"]["location"]["lat"] + "&lon=" + resp["results"][0]["geometry"]["location"]["lng"] + "&radius=1&radius_units=km&format=json&nojsoncallback=1");
     // &radius=20&radius_units=mi
+
+    // Gets results by WOEID/tag
     // var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&woe_id=" + resp["places"]["place"][0]["woeid"] + "&tags=landmark&format=json&nojsoncallback=1");
+
+    // 
+    // var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&woe_id=&tags=park%2C+nature%2C+landscape%2C+orange&tag_mode=all&sort=interestingness-asc&format=json&nojsoncallback=1");
+    var URI = encodeURI(searchValue);
+    var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&text=" + URI + "&format=json&nojsoncallback=1");
     console.log(resp);
 
     resp
@@ -41,7 +47,8 @@ function photoSearch(resp) {
 function createPicture(resp) {
     var pictureArray = resp["photos"]["photo"];
     var $pictureContainer = $('<div></div>', {
-        'class': 'picture-container'
+        'class': 'picture-container',
+        'data-role': 'picture-container'
     })
     pictureArray.forEach(function(picture, i) {
         var $picture = makePicture(picture["farm"], picture["server"], picture["id"], picture["secret"], picture["title"]);
@@ -69,9 +76,18 @@ function addSearchListener() {
     $searchField.on("submit", function (event) {
         event.preventDefault();
         var searchValue = $('[data-role="search"]').val();
-        getGeoCoords(searchValue);
+        // getGeoCoords(searchValue);
+        photoSearch(searchValue);
     });
 }
+
+function addPictureListener() {
+    $('[data-draw="display"]').on('click', $('a'), function(event) {
+    event.preventDefault();
+    deleteOrder($(event.target));
+    })
+}
+
 
 // initMap();
 addSearchListener();
