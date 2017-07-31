c5b7bf717a2f2a9d7b9371f5ebb44d57363818ef
Mac User <sabbey37@gmail.com>
2017-07-31 09:56:25 -0400

    Update spacing

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -31,7 +31,7 @@ function photoSearch(resp, tags) {
     // gets tags from checkbox
     var tags = chooseTags();
     // Adds in tags. Tags are essential in the search process,as well as radius units. These aspects will be changed later to get respnoses from the user
-    var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&lat=" + resp["results"][0]["geometry"]["location"]["lat"] + "&lon=" + resp["results"][0]["geometry"]["location"]["lng"]+ "&tags=" + tags + "&tag_mode=any&radius=20&radius_units=mi&format=json&nojsoncallback=1");
+    var resp = $.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&lat=" + resp["results"][0]["geometry"]["location"]["lat"] + "&lon=" + resp["results"][0]["geometry"]["location"]["lng"] + "&tags=" + tags + "&tag_mode=any&radius=20&radius_units=mi&format=json&nojsoncallback=1");
     resp
         .then(createPicture)
 }
@@ -169,7 +169,7 @@ function placePicMarker(latLon, resp, picInfo) {
     var content = '<div class="iw-container">' + '<h6>' + formatted_address + '</h6>' + '<div class="iw-options">' + '<a target="_blank" rel="noopener noreferrer" href=' + link + '>Directions</a>' + save + '<a href=' + link + + '</div>' + '</div>';
     var icon = 'resources/images/markiethemarker.png';
 
-	  var marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
         position: latLon,
         map: map,
         icon: icon,
