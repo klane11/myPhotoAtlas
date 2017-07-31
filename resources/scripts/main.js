57016c51324d74454a9393c083675c8ab3f43f56
Mac User <sabbey37@gmail.com>
2017-07-31 16:42:52 -0400

    Removes circular structure function

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -310,49 +310,6 @@ function printIt(thing) {
     console.log(thing);
 }
 
-// Allows for circular structure in JSON
-function stringifyOnce(obj, replacer, indent) {
-    var printedObjects = [];
-    var printedObjectKeys = [];
-
-    function printOnceReplacer(key, value){
-        if ( printedObjects.length > 2000){ 
-        return 'object too long';
-        }
-        var printedObjIndex = false;
-        printedObjects.forEach(function(obj, index){
-            if(obj===value){
-                printedObjIndex = index;
-            }
-        });
-
-        if ( key == ''){ //root element
-             printedObjects.push(obj);
-            printedObjectKeys.push("root");
-             return value;
-        }
-
-        else if(printedObjIndex+"" != "false" && typeof(value)=="object"){
-            if ( printedObjectKeys[printedObjIndex] == "root"){
-                return "(pointer to root)";
-            }else{
-                return "(see " + ((!!value && !!value.constructor) ? value.constructor.name.toLowerCase()  : typeof(value)) + " with key " + printedObjectKeys[printedObjIndex] + ")";
-            }
-        }else{
-
-            var qualifiedKey = key || "(empty key)";
-            printedObjects.push(value);
-            printedObjectKeys.push(qualifiedKey);
-            if(replacer){
-                return replacer(key, value);
-            }else{
-                return value;
-            }
-        }
-    }
-    return JSON.stringify(obj, printOnceReplacer, indent);
-};
-
 // ******************************
 // *******HAMBURGER MENU*********
 // ******************************
